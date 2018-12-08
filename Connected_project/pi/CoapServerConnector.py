4693426f0789c4bf8f99c2bc82c70cb5bf3dc857
sujay joshi <40721368+joshisujay1996@users.noreply.github.com>
2018-12-08 10:21:38 -0500

    Update CoapServerConnector.py

--- Connected_project/pi/CoapServerConnector.py
+++ Connected_project/pi/CoapServerConnector.py
@@ -3,11 +3,12 @@ import ConfigUtil
 import ConfigConst
 import CoapResourceHandler
 
-
+#this class implements coap class
 class CoapServerConnector(CoAP):
 
     config=None
-
+#constructors for the class
+#the ip address is given to say it forver one in world can access
     def __init__(self, ipAddr = "0.0.0.0", port = 5683, multicast = False):
         self.config = ConfigUtil.ConfigUtil(ConfigConst.DEFAULT_CONFIG_FILE_NAME)
         self.config.loadConfig()
@@ -16,16 +17,18 @@ class CoapServerConnector(CoAP):
         if port >= 1024:
             self.port = port
         else:
+            #port number to connect by default
             self.port = 5683
         self.ipAddr   = ipAddr
         self.useMulticast = multicast
         self.initResources()
 
     def TestCoapResource(self):
+        #just to test
         print("Testing")
 
 
-
+#adding the resoure with value given is "get_temprature" 
     def initResources(self):
         self.add_resource('get_temperature', CoapResourceHandler.TestCoapResource())
         print("CoAP server started. Binding: " + self.ipAddr + ":" + str(self.port))
