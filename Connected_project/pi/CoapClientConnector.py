5c7cf2ecf57a840448a7b5b1152b1e5786d0d629
sujay joshi <40721368+joshisujay1996@users.noreply.github.com>
2018-12-08 10:30:27 -0500

    Update CoapClientConnector.py

--- Connected_project/pi/CoapClientConnector.py
+++ Connected_project/pi/CoapClientConnector.py
@@ -1,16 +1,18 @@
 from coapthon.client.helperclient import HelperClient
 import ConfigUtil
 import ConfigConst
+
+#creating a class which is used in order to connect to the server
 class CoapClient(object):
     config = None
     serverAddr = None
     host = "localhost"
     port = 5683
 
+    #constructor
     def __init__(self):
         self.config=ConfigUtil.ConfigUtil(ConfigConst.DEFAULT_CONFIG_FILE_NAME)
         self.config.loadConfig()
-
         self.host = self.config.getProperty(ConfigConst.COAP_CLOUD_SECTION, ConfigConst.CLOUD_COAP_HOST)
         self.port = int(self.config.getProperty(ConfigConst.COAP_CLOUD_SECTION.CLOUD_COAP_PORT))
 
@@ -23,7 +25,7 @@ class CoapClient(object):
         if self.port < 1024 or self.port>65535:
             print("using default port: "+ self.port)
 
-
+#adding all parts and protocol used is coap
         self.serverAddr = (self.host, self.port)
         self.url = "coap://"+self.host+":"+str(self.port)
 
@@ -36,7 +38,7 @@ class CoapClient(object):
         except Exception:
             print("Failed to Connect to client: "+ self.host)
             pass
-
+#to handle get request
     def GetRequestHandler(self,resource):
         print("Testing GET for resource: "+ resource)
 
@@ -53,7 +55,7 @@ class CoapClient(object):
 
         self.client.stop()
 
-
+# to handle post request
     def PostRequestHandler(self, resource, payload):
         print("Test POST for resource: "+ resource)
 
@@ -70,7 +72,7 @@ class CoapClient(object):
 
         self.client.stop()
 
-
+#put request
     def PutRequestHandler(self, resource, payload):
         print("Test PUT for resource: "+ resource)
 
@@ -86,7 +88,7 @@ class CoapClient(object):
 
         self.client.stop()
 
-
+#delete request
     def DeleteRequestHandler(self, resource, payload):
         print("Testing DELETE for resource: "+ resource)
 
@@ -102,7 +104,7 @@ class CoapClient(object):
 
         self.client.stop()
 
-
+#ping to the server
     def ServerPing(self):
 
         print("ping to server")
