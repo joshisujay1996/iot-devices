e174fc4535ebc9c89df79068144097a6cd12501b
sujay joshi <40721368+joshisujay1996@users.noreply.github.com>
2018-12-08 10:11:35 -0500

    Update SensorData.py

--- Connected_project/pi/SensorData.py
+++ Connected_project/pi/SensorData.py
@@ -12,26 +12,26 @@ class SensorData():
     def __init__(self):
         self.timeStamp = str(datetime.now())
 
-    #This function tracks every new temperature reading, calculates the average.
+    #This function tracks every new temperature reading, calculates the average
     def addValue(self, newVal):
 
         #increase the sampleCount for every reading
         self.sampleCount += 1
 
-        #Adds the new temperature reading to the totalValue and notes the time of the reading
+        #adds the new temperature reading to the totalValue and notes the time of the reading
         self.totValue = self.totValue+newVal;
         self.timeStamp = str(datetime.now())
         self.curValue= newVal
 
-        #Tracks the minimum recorded temperature
+        #tracks the minimum recorded temperature
         if(self.curValue < self.minValue):
             self.minValue = self.curValue
 
-        #Tracks the maximum recorded temperature
+        #tracks the maximum recorded temperature
         if(self.curValue>self.maxValue):
             self.maxValue = self.curValue
 
-        #Calculate the average from all the readings (update after every reading)
+        #calculate the average from all the readings
         if(self.totValue!=0 and self.sampleCount>0):
             self.avgValue = self.totValue/self.sampleCount
 
