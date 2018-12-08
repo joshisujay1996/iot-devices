fb662120541d5a21b07ba525c5a63b2b5cf71655
sujay joshi <40721368+joshisujay1996@users.noreply.github.com>
2018-12-08 10:14:44 -0500

    Update SensorAdaptor.py

--- Connected_project/pi/SensorAdaptor.py
+++ Connected_project/pi/SensorAdaptor.py
@@ -3,13 +3,16 @@ from time import sleep
 import CoapClientApp
 import senseme
 
+#this class has a function whilch will be used in other class to call and run all the sensors and get values
 class SensorAdaptor():
     def runme(self):
         while 1:
             temp = senseme.sensorFunction()
+            #check if temp is greater than 25 , if so call teh client app to send data
             if(temp>25):
                 CoapClientApp.startApp()
-            elif(senseme,intruderFunction()==1):
+             #check if acclerometer reading , if so call teh client app to send data
+            elif(senseme.intruderFunction()==1):
                 CoapClientApp.startApp()
             else:
                 sense.clear()
