123abcc03fa4f7861ac4883d2765b219778f4659
sujay joshi <40721368+joshisujay1996@users.noreply.github.com>
2018-12-08 10:45:37 -0500

    Update CoapServerConnector.py

--- Connected_project/pc/CoapServerConnector.py
+++ Connected_project/pc/CoapServerConnector.py
@@ -3,11 +3,11 @@ import ConfigUtil
 import ConfigConst
 from CoapResourceHandler import CoapResource
 
-
+#this is used in the coap server app to run the server
 class CoapServerConnector(CoAP):
 
     config=None
-
+#constructor
     def __init__(self, ipAddr = "0.0.0.0", port = 5683, multicast = False):
         self.config = ConfigUtil.ConfigUtil(ConfigConst.DEFAULT_CONFIG_FILE_NAME)
         self.config.loadConfig()
@@ -20,12 +20,12 @@ class CoapServerConnector(CoAP):
         self.ipAddr   = ipAddr
         self.useMulticast = multicast
         self.initResources()
-
+#tesing
     def CoapResource(self):
         print("Testing")
 
 
-
+#adding resource with value get_temprature
     def initResources(self):
         self.add_resource('get_temperature', CoapResourceHandler.CoapResource())
         print("CoAP server started. Binding: " + self.ipAddr + ":" + str(self.port))
