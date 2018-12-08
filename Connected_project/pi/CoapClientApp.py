da270470f55d804429c49fed06fd3772614b89a4
sujay joshi <40721368+joshisujay1996@users.noreply.github.com>
2018-12-08 10:32:27 -0500

    Update CoapClientApp.py

--- Connected_project/pi/CoapClientApp.py
+++ Connected_project/pi/CoapClientApp.py
@@ -2,9 +2,14 @@
 from time import sleep
 import CoapClientConnector
 
+#this is call when we wan to start the application
 def startApp():
+    #calling the client connector
     clientApp = CoapClientConnector.CoapClient()
+    #resoure her is get_temprature saame as taht of server
     resource = "get_temperature"
+    #payload is what the data which needs to be sent
     payload = "Coapclinet in RPi to CoapServer in gateway"
+    #below handling post and get requests
     clientApp.PostRequestHandler(resource, payload)
     clientApp.GetRequestHandler(resource)
